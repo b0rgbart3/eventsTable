6a5e0d7194272e96246b6f210c8bdf8de7e2429f
Bart Dority <b0rgBart3@gmail.com>
2022-09-11 18:20:15 -0700

    adding express

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const http = require('http');
+var express = require('express');
+var app     = express();
 
-const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
-}
+app.set('port', (process.env.PORT || 5000));
 
-const server = http.createServer(requestListener);
-server.listen(8080);
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
+// const requestListener = function (req, res) {
+//   res.writeHead(200);
+//   res.end('Hello, World!');
+// }
+
+// const server = http.createServer(requestListener);
+// server.listen(8080);
