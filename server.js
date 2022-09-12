7c7024053bb5684cfeeb78bdef74e9dd1c4e906c
Bart Dority <b0rgBart3@gmail.com>
2022-09-12 16:48:33 -0700

    heroku port error

--- server.js
+++ server.js
@@ -66,14 +66,14 @@ app.get("/api/events", (req, res, next) => {
   res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
-//For avoidong Heroku $PORT error
-// app.set('port', (process.env.PORT || 5000));
-// app.get('/', function(request, response) {
-//     var result = 'App is running'
-//     response.send(result);
-// }).listen(
+// For avoidong Heroku $PORT error
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(
   
-//   app.get('port'), function() {
-//     console.log('App is running, server is listening on port ', app.get('port'));
-// });
+  app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
