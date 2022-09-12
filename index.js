b985ab79ed0603478c48d4f74276373732bdf393
Bart Dority <b0rgBart3@gmail.com>
2022-09-11 18:26:43 -0700

    simplify express server

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const http = require('http');
-var express = require('express');
-var app     = express();
 
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+var express = require("express");
+var app = express();
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
 });
 
-// const requestListener = function (req, res) {
-//   res.writeHead(200);
-//   res.end('Hello, World!');
-// }
 
-// const server = http.createServer(requestListener);
-// server.listen(8080);
+// app.set('port', (process.env.PORT || 5000));
+
+// //For avoidong Heroku $PORT error
+// app.get('/', function(request, response) {
+//     var result = 'App is running'
+//     response.send(result);
+// }).listen(
+  
+//   app.get('port'), function() {
+//     console.log('App is running, server is listening on port ', app.get('port'));
+// });
+
