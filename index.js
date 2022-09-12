7220e9044ae974fa20b455c7d7fba81cd5c8829e
Bart Dority <b0rgBart3@gmail.com>
2022-09-11 18:30:20 -0700

    fixing port issue

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 
+app.get("/api/data", (req, res, next) => {
+  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+ });
 
-// app.set('port', (process.env.PORT || 5000));
 
-// //For avoidong Heroku $PORT error
-// app.get('/', function(request, response) {
-//     var result = 'App is running'
-//     response.send(result);
-// }).listen(
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(
   
-//   app.get('port'), function() {
-//     console.log('App is running, server is listening on port ', app.get('port'));
-// });
+  app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
